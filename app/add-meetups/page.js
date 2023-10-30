6c4479c33b354163aae1620f1e4cc980232ebdc0
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-10-30 11:15:48 +0530

    Making the add meetings porperly with abit of functionality.

--- app/add-meetups/page.js
+++ app/add-meetups/page.js
@@ -1,10 +1,33 @@
 "use client";
+import { useDispatch } from "react-redux";
+import { addMeetup } from "../store/Features/slices/page";
+
 const AddMeetups = () => {
+    
+    const dispatch = useDispatch();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        //making an object to store the data of new meetup
+        const newMeetups = {
+            id: Date.now(), //unique ID
+            name: event.target.meetingName.value,
+            address: event.target.meetingAddress.value,
+            time: event.target.meetingTime.value,
+            image: event.target.meetingImage.value,
+            descriptipn: event.target.meetingDescription.value,
+        }
+
+        dispatch(addMeetup(newMeetups));
+        console.log("Meetup: , newMeetups");
+    }
+
   return (
     <div className="bg-slate-300 min-h-screen flex flex-col items-center justify-center">
       <div className="bg-sky-900 text-white text-2xl italic p-4 w-full text-center">Add Meetups</div>
       <div className="bg-white p-8 rounded shadow-lg w-full max-w-md">
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
             <label className="text-gray-800 font-semibold" htmlFor="meeting-name">
               Meeting Name:
@@ -12,6 +35,7 @@ const AddMeetups = () => {
             <input
               type="text"
               id="meeting-name"
+              name="meetingName"
               className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:ring-sky-300"
             />
           </div>
@@ -22,6 +46,7 @@ const AddMeetups = () => {
             <input
               type="text"
               id="meeting-address"
+              name="meetingAddress"
               className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:ring-sky-300"
             />
           </div>
@@ -32,6 +57,7 @@ const AddMeetups = () => {
             <input
               type="time"
               id="meeting-time"
+              name="meetingTime"
               className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:ring-sky-300"
             />
           </div>
@@ -42,6 +68,7 @@ const AddMeetups = () => {
             <input
               type="text"
               id="image-url"
+              name="meetingImage"
               className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:ring-sky-300"
             />
           </div>
@@ -52,6 +79,7 @@ const AddMeetups = () => {
             <input
               type="text"
               id="meeting-description"
+              name="meetingDescription"
               className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring focus:ring-sky-300"
             />
           </div>
